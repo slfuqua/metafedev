2226eccce077f0bf57d6e15568062cc7b5ea7d04
slfuqua <sl@fuqua.net>
2025-07-24 15:16:16 -0500

    Add js file for loops and update .gitignore, which is not working.

--- js/loops.js
+++ js/loops.js
@@ -20,16 +20,16 @@
 //   i++;
 // }
 
-for (let i = 1; i <=5; i++) {
-  console.log(i);
-  if (i === 5) {
-    console.log('Counting completed!')
-  }
+for (let i = 1; i <= 5; i++) {
+	console.log(i);
+	if (i === 5) {
+		console.log("Counting completed!");
+	}
 }
 
 for (i = 5; i > 0; i--) {
-  console.log(i);
-  if (i === 1) {
-    console.log('Countdown finished!')
-  }
+	console.log(i);
+	if (i === 1) {
+		console.log("Countdown finished!");
+	}
 }
