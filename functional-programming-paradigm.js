e851c557efd9e7f13a795a291974dfed9d0aa512
hikinthru <hikinthru@gmail.com>
2025-08-01 18:48:44 -0500

    Update build-a-functional-program.js

---
+++ functional-programming-paradigm.js
@@ -0,0 +1,20 @@
+/*
+Learning Objectives
+By the end of this reading, you will:
+
+- Be able to explain that there are several programming paradigms
+
+- Be able to explain the basic difference between the two predominant programming paradigms: functional programming and object-oriented programming
+
+- Understand, at a very high level, how the functional programming paradigm works
+*/
+
+function getTotal(a, b) {
+  return a + b;
+}
+
+const num1 = 2;
+const num2 = 3;
+
+let total = getTotal(num1, num2);
+console.log(total); // Output: 5
