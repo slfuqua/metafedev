dd1a900deb7b6c560e355e4e091b54a32485f8c5
hikinthru <hikinthru@gmail.com>
2025-08-21 12:07:51 -0500

    Add objects-and-for-loops.js

--- js/mod-3-programming-paradigms/tempCodeRunnerFile.js
+++ js/mod-3-programming-paradigms/tempCodeRunnerFile.js
@@ -1,24 +1,3 @@
-const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
-
-// for (let color of colors) {
-//   console.log(color)
-// }
-
-// const car = {
-//   speed: 200,
-//   color: 'red'
-// }
-// console.log(Object.keys(car)); // [ 'speed', 'color' ]
-// console.log(Object.values(car)); // [ 200, 'red' ]
-// console.log(Object.entries(car)); // [ [ 'speed', 200 ], [ 'color', 'red' ] ]
-
-// var clothingItem = {
-//     price: 50,
-//     color: 'beige',
-//     material: 'cotton',
-//     season: 'autumn'
-// }
-
-// for (const key of Object.keys(clothingItem)) {
-//   console.log(`${key}: ${clothingItem[key]}`)
+for (const key in car) {
+//   console.log(`${key}: ${car[key]}`);
 // }
