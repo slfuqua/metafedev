02d3865514bb0da5fd180a382879a651d69d4495
slfuqua <sl@fuqua.net>
2025-09-14 10:04:34 -0500

    Move the aside to the right

--- uomwebdev/script.js
+++ uomwebdev/script.js
@@ -6,3 +6,9 @@ const button1 = document.getElementById("button1");
 button1.addEventListener("click", () => {
 	ex1.innerText = "My new text using <code>innerHTML</code>";
 });
+
+const ex2 = document.getElementById("ex2");
+const button2 = document.getElementById("button2");
+button2.addEventListener("click", () => {
+	return console.log(ex2);
+});
