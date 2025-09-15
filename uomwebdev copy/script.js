05905fdcbca990f7c901576ef056f6a1600ae17f
hikinthru <hikinthru@gmail.com>
2025-09-15 14:02:30 -0500

    Restore styles from Friday; remove backup copy

--- uomwebdev copy/script.js
+++
@@ -1,8 +0,0 @@
-// Using  getElementById and innerHTML
-// document.getElementById("demo").innerHTML = "Hello World!";
-
-const ex1 = document.getElementById("ex1");
-const button1 = document.getElementById("button1");
-button1.addEventListener("click", () => {
-	ex1.innerText = "My new text using <code>innerHTML</code>";
-});
